90583b7ac417dbb20a414e33ef22ae96ccdef6fe
Jörn Loviscach <joern.loviscach@hotmail.de>
2018-06-07 16:57:15 +0200

    Stand nach Veranstaltung

--- SignaleAlsKlassenMitVererbung/SignaleAlsKlassenMitVererbung/MainWindow.xaml.cs
+++ SignaleAlsKlassenMitVererbung/SignaleAlsKlassenMitVererbung/MainWindow.xaml.cs
@@ -31,7 +31,17 @@ namespace SignaleAlsKlassenMitVererbung
 
         private void buttonZeichne_Click(object sender, RoutedEventArgs e)
         {
+            // upcast: immer sicher, Casting nicht nötig
             Signal s = new Sinusschwingung(13.0, 5.0);
+            Signal t23 = new Rechteckschwingung(11.0, 5.0);
+            Signal u = new Summe(s, 0.5, t23, 0.3);
+            Signal v = new Summe(u, 0.5, t23, -0.1);
+            //Signal w = new Ableitung(v);
+            double x42 = s.SchätzeNennwert(13.0, 42.0, 0.0001);
+
+            // downcast: nicht sicher, Casting nötig
+            // Exception mit: Sinusschwingung z = (Sinusschwingung)t23;
+
             Zeichenfläche.Children.Clear();
             Polyline p = new Polyline();
             p.Stroke = Brushes.Blue;
@@ -41,7 +51,7 @@ namespace SignaleAlsKlassenMitVererbung
             {
                 double t = 0.001 * i;
                 double x = Zeichenfläche.Width * i / 1000;
-                double y = Zeichenfläche.Height * (0.5 - 0.1 * s.GibWert(t));
+                double y = Zeichenfläche.Height * (0.5 - 0.1 * v.GibWert(t));
                 p.Points.Add(new Point(x, y));
             }
         }
@@ -98,6 +108,28 @@ namespace SignaleAlsKlassenMitVererbung
         {
             return 0.0;
         }
+
+        public double SchätzeNennwert(double startZeit,
+            double endZeit, double schrittweite)
+        {
+            double summe = 0.0;
+            int anzahlSchritte = 0;
+            // Problem: Rundungsfehler für t?
+            // Lieber keine double-Zahl in Bedingung von for-Schleife
+            for (double t = startZeit; t < endZeit; t += schrittweite)
+            {
+                double y = GibWert(t);
+                summe += y * y;  // Oder: Math.Pow(y, 2); nebenbei: y^2 ist XOR
+                anzahlSchritte++;
+            }
+            double mittelwert = summe / anzahlSchritte;
+            return Math.Sqrt(mittelwert);
+        }
+
+        public override string ToString()
+        {
+            return "mein Signal";
+        }
     }
 
     class Sinusschwingung : Signal
@@ -114,6 +146,68 @@ namespace SignaleAlsKlassenMitVererbung
         public override double GibWert(double t)
         {
             return amplitude * Math.Sin(2.0 * Math.PI * frequenz * t);
+        }
+
+        public override string ToString()
+        {
+            return "Sinusschwingung mit Frequenz " + frequenz + " und Amplitude " + amplitude;
+        }
+    }
+
+    class Rechteckschwingung : Signal
+    {
+        double frequenz;
+        double amplitude;
+
+        public Rechteckschwingung(double frequenz, double amplitude)
+        {
+            this.frequenz = frequenz;
+            this.amplitude = amplitude;
+        }
+
+        public override double GibWert(double t)
+        {
+            double perioden = frequenz * t;
+            double gebrochenePerioden = perioden - Math.Floor(perioden);
+            // Aber Vorsicht mit Aliasing!
+            return amplitude * (gebrochenePerioden > 0.5 ? -1.0 : 1.0);
+        }
+    }
+
+    class Summe : Signal
+    {
+        Signal s1;
+        double faktor1;
+        Signal s2;
+        double faktor2;
+
+        public Summe(Signal s1, double faktor1, Signal s2, double faktor2)
+        {
+            this.s1 = s1;
+            this.faktor1 = faktor1;
+            this.s2 = s2;
+            this.faktor2 = faktor2;
+        }
+
+        public override double GibWert(double t)
+        {
+            return faktor1 * s1.GibWert(t) + faktor2 * s2.GibWert(t);
+        }
+    }
+
+    class Ableitung : Signal
+    {
+        Signal s;
+
+        public Ableitung(Signal s)
+        {
+            this.s = s;
+        }
+
+        public override double GibWert(double t)
+        {
+            const double deltaT = 1e-10;
+            return (s.GibWert(t + deltaT) - s.GibWert(t - deltaT)) / (2.0 * deltaT);
         }
     }
 }
